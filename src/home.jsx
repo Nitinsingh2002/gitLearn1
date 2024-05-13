74df600cdf37a045cbab9646480b6980832df76f
Nitin kumar singh <singhnitinkumar145@gmail.com>
2024-05-14 00:12:03 +0530

    this is first commit bu suman on devloper brancg

--- src/home.jsx
+++ src/home.jsx
@@ -5,6 +5,10 @@ export  const Home =()=>{
     return(
         <>
         this is Task HIS -134
+
+           
+           <h1>this task is  for develope branch </h1>
+           <h2>this is completed by suman</h2>
         </>
     )
 }
