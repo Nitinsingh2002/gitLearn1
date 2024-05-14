1a28ab72e0aee73c181135735e3c481692f87f6d
Nitin kumar singh <singhnitinkumar145@gmail.com>
2024-05-14 12:10:44 +0530

    task 301 is completetd sucessfully

--- src/his.jsx
+++ src/his.jsx
@@ -7,6 +7,12 @@ export function His (){
         <>
           hello team 
           i have completetd HIS543  task
+
+
+          <div>
+            this is HiS-301 
+            this task is completed by sarubh
+          </div>
         </>
     )
 }
